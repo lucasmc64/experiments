b7cbcea08b09f626c41a7fe0a1fb2b0a336514e6
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2020-10-26 20:52:20 -0300

    Improved implementation

--- sorting-algorithms/bubblesort.js
+++ sorting-algorithms/bubblesort.js
@@ -1,27 +1,29 @@
 let exemple = [20, 8, 2, 11, 13, 3, 7, 18, 14, 4, 16, 10, 15, 1, 9, 17, 19, 12, 5, 6];
 
 /*
-    Essa implementação é apenas um teste. 
-    Ela não está otimizada pois:
-        * não para quando está ordenado, ou seja, vai se chamar recursivamente as `n` vezes;
+    Implementação baseada no que aprendi nas disciplinas de Programação Funcional e Algoritmos e Estruturas de Dados 1 e 2.
 */
 
 let bubblesortAux = (arr, n) => {
-    if(n == 0) {
-        return arr;
-    } else {
-        let aux;
-    
-        for(let i = 0; i < n; i++) {
+
+    let aux, proceed, end = n;
+
+    do {
+        proceed = 0;
+
+        for(let i = 0; i < end - 1; i++) {
             if(arr[i] > arr[i + 1]) {
                 aux = arr[i + 1];
                 arr[i + 1] = arr[i];
                 arr[i] = aux;
+                proceed = i;
             }
         }
 
-        return bubblesortAux(arr, n - 1);
-    }
+        end--;
+    } while(proceed != 0);
+
+    return arr; 
 }
 
 let bubblesort = (arr) => {
