0626b25d889dfcc085031b75593d5638a64cb779
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2020-10-26 19:45:38 -0300

    file renaming and a code comment

--- sorting-algorithms/quicksort.js
+++ sorting-algorithms/quicksort.js
@@ -1,5 +1,12 @@
 let exemple = [20, 8, 2, 11, 13, 3, 7, 18, 14, 4, 16, 10, 15, 1, 9, 17, 19, 12, 5, 6];
 
+/*
+    Essa implementação é apenas um teste. 
+    Ela não está otimizada pois:
+        * não escolhe o pivô ideal;
+        * não foi pensado nas possíveis diferenças de otimização do uso do `.concat` e `.push`.
+*/
+
 let findAndDeletePivo = (arr) => {
     let pivo = arr.shift();
     return pivo;
