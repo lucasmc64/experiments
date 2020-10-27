f9688c42202c2b9be5370116ba9c5f5a13604d50
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2020-10-26 21:15:38 -0300

    2 versions of same idea

--- sorting-algorithms/bubblesort.js
+++ sorting-algorithms/bubblesort.js
@@ -4,7 +4,9 @@ let exemple = [20, 8, 2, 11, 13, 3, 7, 18, 14, 4, 16, 10, 15, 1, 9, 17, 19, 12, 
     Implementação baseada no que aprendi nas disciplinas de Programação Funcional e Algoritmos e Estruturas de Dados 1 e 2.
 */
 
-let bubblesortAux = (arr, n) => {
+// Versão "Linear"
+
+let bubblesortAuxL = (arr, n) => {
     let aux, proceed, end = n;
 
     do {
@@ -25,8 +27,33 @@ let bubblesortAux = (arr, n) => {
     return arr; 
 }
 
-let bubblesort = (arr) => {
-    return bubblesortAux(arr, arr.length);
+let bubblesortL = (arr) => {
+    return bubblesortAuxL(arr, arr.length);
 }
 
-console.log(bubblesort(exemple));
+// Versão "Funcional"
+
+let bubblesortAuxF = (arr, n) => {
+    if(n == 0) {
+        return arr;
+    } else {
+        let aux, count_changes = 0;
+
+        for(let i = 0; i < n; i++) {
+            if(arr[i] > arr[i + 1]) {
+                aux = arr[i + 1];
+                arr[i + 1] = arr[i];
+                arr[i] = aux;
+                count_changes++;
+            }
+        }
+
+        return !count_changes ? arr : bubblesortAuxF(arr, n - 1);
+    }
+}
+
+let bubblesortF = (arr) => {
+    return bubblesortAuxF(arr, arr.length);
+}
+
+console.log(bubblesortL(exemple));
