c3ccb7a202345ef4ba9c49eed1dd7c901dc1c1b3
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2021-07-12 11:41:42 -0300

    Lazy Loading Images

--- bankist-website/script.js
+++ bankist-website/script.js
@@ -146,6 +146,34 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
 });
 
+// Lazy Loading Images
+
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+
+  entry.target.src = entry.target.dataset.src; // Assim que terminar de carregar a imagem, o JS dispara o evento de load.
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  rootMargin: '200px',
+  threshold: 0,
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
 /*
 // Mensagem de Cookies
 const header = document.querySelector('.header');
