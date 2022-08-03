f4c83feb29732ea874d7ccfb6ae398f6c55b4030
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2022-08-03 07:32:01 -0400

    Start study Node

---
+++ natours-api/app.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (request, response) => {
+  response.status(200).json({ status: 'success', data: { tours } });
+});
+
+app.get('/api/v1/tours/:id', (request, response) => {
+  response.status(200).json({ status: 'success', data: { tours } });
+});
+
+app.post('/api/v1/tours', (request, response) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, request.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (error) => {
+      response.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}!`);
+});
